08650eb4901f793a9024e528c09119bb9d69ac20
Heri Sarwanto <heri.sarwanto@majoo.id>
2022-12-04 20:27:01 +0700

    Edit analysis_options.yaml

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_movie/counter/counter.dart';
+import 'package:my_movie/features/movie/presentation/pages/home_page.dart';
 import 'package:my_movie/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -16,7 +16,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const HomePage(),
     );
   }
 }
